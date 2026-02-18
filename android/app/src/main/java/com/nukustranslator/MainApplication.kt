be6ce6641949134b66b94eadb815bfb048e198ad
timur01261989 <timur012689@gmail.com>
2026-02-19 01:55:55 +0500

    Update MainApplication.kt

--- android/app/src/main/java/com/nukustranslator/MainApplication.kt
+++ android/app/src/main/java/com/nukustranslator/MainApplication.kt
@@ -3,35 +3,45 @@ package com.nukustranslator
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
+/**
+ * NOTE:
+ * - New Architecture is controlled by android/gradle.properties -> newArchEnabled=false
+ * - If you enable it, make sure all required native .so are packaged, otherwise you'll crash with:
+ *   UnsatisfiedLinkError: library "libreact_featureflagsjni.so" not found
+ */
 class MainApplication : Application(), ReactApplication {
 
-  private val mReactNativeHost: ReactNativeHost =
+  override val reactNativeHost: ReactNativeHost =
     object : DefaultReactNativeHost(this) {
-      override fun getPackages() =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here
-          add(BridgePackage())
-        }
+
+      override fun getPackages(): List<ReactPackage> =
+        PackageList(this).packages
 
       override fun getJSMainModuleName(): String = "index"
 
       override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
       override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+
       override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
     }
 
-  override val reactNativeHost: ReactNativeHost
-    get() = mReactNativeHost
+  override val reactHost: ReactHost
+    get() = DefaultReactHost.getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
+    SoLoader.init(this, /* native exopackage */ false)
+
+    // This will only run if newArchEnabled=true at build time.
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       DefaultNewArchitectureEntryPoint.load()
     }
