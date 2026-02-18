413f5f90a56353f8c021e6d57c5cfa080a30b6b5
timur01261989 <timur012689@gmail.com>
2026-02-18 23:25:35 +0500

    Create MainApplication.kt

---
+++ android/app/src/main/java/com/nukustranslator/MainApplication.kt
@@ -0,0 +1,10 @@
+package com.nukustranslator
+
+import android.app.Application
+import android.content.Context
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
