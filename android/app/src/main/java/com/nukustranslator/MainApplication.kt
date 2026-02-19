69b9356f22c82e32dcb11d6f413f62936313560b
timur01261989 <timur012689@gmail.com>
2026-02-19 09:36:06 +0500

    Update MainApplication.kt

--- android/app/src/main/java/com/nukustranslator/MainApplication.kt
+++ android/app/src/main/java/com/nukustranslator/MainApplication.kt
@@ -3,20 +3,11 @@ package com.nukustranslator
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
-import com.facebook.react.defaults.DefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
-/**
- * NOTE:
- * - New Architecture is controlled by android/gradle.properties -> newArchEnabled=false
- * - If you enable it, make sure all required native .so are packaged, otherwise you'll crash with:
- *   UnsatisfiedLinkError: library "libreact_featureflagsjni.so" not found
- */
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
@@ -29,21 +20,15 @@ class MainApplication : Application(), ReactApplication {
 
       override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-      override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      // New Architecture-ni MAJBURAN o‘chiramiz (gradle flag noto‘g‘ri bo‘lsa ham)
+      override val isNewArchEnabled: Boolean = false
 
       override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
     }
 
-  override val reactHost: ReactHost
-    get() = DefaultReactHost.getDefaultReactHost(applicationContext, reactNativeHost)
-
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, /* native exopackage */ false)
-
-    // This will only run if newArchEnabled=true at build time.
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      DefaultNewArchitectureEntryPoint.load()
-    }
+    SoLoader.init(this, false)
+    // DefaultNewArchitectureEntryPoint.load() QATTIQ chaqirilmaydi
   }
 }
